f09c89c276e1501d795a302740d0a5d5a90cbc49
EnricoMiccoli <EnricoMiccoli@users.noreply.github.com>
2018-12-02 13:14:59 +0100

    Implement voltage generators

--- nodal.py
+++ nodal.py
@@ -10,22 +10,28 @@ VCOL = 2 # value of component, eg resistance
 ACOL = 3 # node connected to first lead, currents enter here
 BCOL = 4 # node connected to second lead
 
+NODE_TYPES = ["A", "R", "E"]
+NODE_TYPES_ANOM = ["E"]
+
 # Compute problem dimensions and find highest degree node
 with open(NETLIST, 'r') as infile:
     netlist = csv.reader(infile)
-    nodes = set()
     ncomps = 0
     degrees = {}
+    anomalies = 0
+    anomnum = {}
     for component in netlist:
         ncomps += 1
         curnodes = component[ACOL:BCOL+1]
         newnodes = [key for key in curnodes if key not in degrees]
+        if component[TCOL] in NODE_TYPES_ANOM:
+            anomnum[component[NCOL]] = anomalies
+            anomalies += 1
         for node in newnodes:
             degrees[node] = 0
         for node in curnodes:
             degrees[node] += 1
 
-    f = (len(degrees), ncomps)
     ground = max(degrees.keys(), key=(lambda x: degrees[x]))
 
     i = 0
@@ -34,15 +40,19 @@ with open(NETLIST, 'r') as infile:
         nodenum[node] = i
         i += 1
     assert len(nodenum) == len(degrees) - 1
+    
 
+# We are going to solve Gx = A
 # Build coefficient matrix 
 with open(NETLIST, 'r') as infile:
-    G = np.zeros(shape=((f[0]-1,)*2))
-    A = np.zeros(shape=((f[0]-1,1)))
+    n = len(degrees) - 1 + anomalies # number of unknowns
+    G = np.zeros(shape=(n, n))
+    A = np.zeros(shape=(n, 1))
     netlist = csv.reader(infile)
+    currents = []
     for component in netlist:
         assert type(component[TCOL]) == str
-        assert component[TCOL] in ["A", "R"]
+        assert component[TCOL] in NODE_TYPES
         anode = component[ACOL]
         bnode = component[BCOL]
         if anode != ground:
@@ -66,14 +76,37 @@ with open(NETLIST, 'r') as infile:
                 A[i] += current
             if bnode != ground:
                 A[j] -= current
+        elif component[TCOL] == "E":
+            tension = float(component[VCOL])
+            k = anomnum[component[NCOL]]
+            i = len(nodenum) + k
+            currents.append(component[NCOL])
+            A[i] += tension
+            if anode != ground:
+                j = nodenum[anode]
+                G[i,j] = 1
+                G[j,i] = -1
+            if bnode != ground:
+                j = nodenum[bnode]
+                G[i,j] = -1
+                G[j,i] = 1
         else:
             exit(1)
 
-e = np.linalg.solve(G, A)
+try:
+    e = np.linalg.solve(G, A)
+except np.linalg.linalg.LinAlgError:
+    print("Model error: matrix is singular")
+    print(G)
+    exit(1)
 
 nodelabel = dict(reversed(x) for x in nodenum.items())
 print("Ground node: {}".format(ground))
 i = 0
-for potential in e:
+for potential in e[0:len(nodenum)]:
     print("e({}) = {}".format(nodelabel[i], potential))
+    i += 1
+i = 0
+for current in e[len(nodenum):len(nodenum)+anomalies]:
+    print("i({}) = {}".format(currents[i], current))
     i += 1
